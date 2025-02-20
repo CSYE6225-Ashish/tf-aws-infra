a84da5fa8074c206e86b763dcee630fcd02b470c
Ashish Babish Gangaramani <gangaramani.a@northeastern.edu>
2025-02-19 19:50:27 -0500

    Changes in readme to trigger CI workflow

--- main.tf
+++ main.tf
@@ -85,6 +85,6 @@ resource "aws_route_table" "private_route_table" {
 resource "aws_route_table_association" "aws_route_table_private" {
   for_each       = local.private_subnet_ids
   subnet_id      = each.value
-  route_table_id = aws_route_table.private_route_table.id
+   route_table_id = aws_route_table.private_route_table.id
 }
 
